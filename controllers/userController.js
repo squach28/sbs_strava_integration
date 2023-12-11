fc8dad7650afedda2343b082a18eb05178129bfe
squach28 <squach3443@gmail.com>
2023-12-11 15:46:13 -0800

    get user stats implemented

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,19 +18,42 @@ const getUserByDiscordId = async (req, res) => {
     }
 }
 
-// uses strava api to get stats about a user
+// provides stats for the user
+// stats - number of activites and distance by activity and total number of activites and distance
 const getStatsByDiscordId = async (req, res) => {
     const discordId = req.query.discordId
-    const stravaId = req.stravaId
     const accessToken = await getAccessToken(discordId)
-
-    const response = await fetch(`https://www.strava.com/api/v3/athletes/${stravaId}/stats`, {
+    
+    const response = await fetch(`https://www.strava.com/api/v3/athlete/activities`, {
         headers: {
             Authorization: `Bearer ${accessToken}`
         }
     })
-    const data = await response.json()
-    res.send(data)   
+    const activities = await response.json()
+    const stats = {}
+    for(let activity of activities) {
+        const sportType = activity.sport_type
+        const distance = convertToMiles(activity.distance)
+        if(!(sportType in stats)) {
+            stats[sportType] = {
+                distance: 0,
+                numOfActivities: 0
+            }
+        }
+        stats[sportType].numOfActivities += 1
+        stats[sportType].distance += distance 
+    }
+    const result = []
+    for(let stat of Object.entries(stats)) {
+        const resultStat = {
+            category: stat[0],
+            distance: stat[1].distance,
+            numOfActivities: stat[1].numOfActivities
+        }
+        result.push(resultStat)
+    }
+    console.log(result)
+    res.send(result)   
 }
 
 // gets user's activities by discord id
