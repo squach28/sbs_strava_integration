e24904fa104075139a44a6adbfdc9d8314249500
squach28 <squach3443@gmail.com>
2023-12-18 14:53:14 -0800

    send error response when user tries stats or activities command without registering

--- middleware/userMiddleware.js
+++ middleware/userMiddleware.js
@@ -6,12 +6,17 @@ const userExistsMiddleware = async (req, res, next) => {
     const discordId = req.query.discordId
     const user = await fetchUserByDiscordId(discordId)
     if(user === null) {
-        res.status(404).json({ message: 'User is not registered'})
+        res.status(404).json({ message: 'User not found, use the /register command to connect your Strava account.' })
     } else { // user exists, pass stravaId, stravaTokenExpiresAt, and stravaRefreshToken into next request
-        req.stravaId = user.stravaId 
-        req.stravaTokenExpiresAt = user.stravaTokenExpiresAt
-        req.stravaRefreshToken = user.stravaRefreshToken
-        next()
+        if(user.sessionId) { // user exists, but sessionId is present -> user hasn't completed oauth2
+            res.status(403).json({ message: 'User found with incomplete registration. Please use the link sent in your DM to finish registration or start a new registration with the /register command.'})
+        } else {
+            req.stravaId = user.stravaId 
+            req.stravaTokenExpiresAt = user.stravaTokenExpiresAt
+            req.stravaRefreshToken = user.stravaRefreshToken
+            next()
+        }
+
     }
 }
 
