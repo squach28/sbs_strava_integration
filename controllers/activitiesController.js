4b484d8dc91123170ff58b494db5ff1443d1a72a
squach28 <squach3443@gmail.com>
2024-01-08 22:36:56 -0800

    support for getting activites by timeframe

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -4,8 +4,10 @@ const querystring = require('querystring')
 const { convertToMiles } = require('../utils/unitsConverter')
 const Activity = require('../models/Activity')
 
+const STRAVA_ACTIVITES_API_URL = 'https://www.strava.com/api/v3/athlete/activities'
+
 // get the activities of all users sorted by start date descending
-const getActivities = async (req, res) => {
+const getAllActivities = async (req, res) => {
     try {
         const activities = await Activity.find({})
             .sort({ startDate: -1 })
@@ -54,6 +56,30 @@ const getRecentActivitiesByDiscordId = async (req, res) => {
     }
 }
 
+const getActivitiesByTimeframe = async (req, res) => {
+    const discordId = req.query.discordId
+    const before = req.query.before
+    const after = req.query.after
+    console.log(discordId)
+    try {
+        const accessToken = await getAccessToken(discordId)
+        const queryParams = {
+            before: before,
+            after: after
+        }
+        const response = await fetch(`${STRAVA_ACTIVITES_API_URL}?${querystring.stringify(queryParams)}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+        const activities = await response.json()
+        res.send(activities)
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({ message: 'Something went wrong, please try again later.'})
+    }
+}
 
 
-module.exports = { getActivities, getActivitiesByDiscordId, getRecentActivitiesByDiscordId }
+
+module.exports = { getAllActivities, getActivitiesByDiscordId, getRecentActivitiesByDiscordId, getActivitiesByTimeframe }
--- routes/activitiesRoutes.js
+++ routes/activitiesRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const { getActivitiesByDiscordId, getActivities, getRecentActivitiesByDiscordId } = require('../controllers/activitiesController')
+const { getActivitiesByDiscordId, getAllActivities, getRecentActivitiesByDiscordId, getActivitiesByTimeframe } = require('../controllers/activitiesController')
 const { userExistsMiddleware } = require('../middleware/userMiddleware')
 const { expiredTokenMiddleware } = require('../middleware/tokenMiddleware')
 const router = express.Router()
 
-router.get('/all', getActivities)
+router.get('/all', getAllActivities)
 router.use('/', userExistsMiddleware)
 router.use('/', expiredTokenMiddleware)
 router.get('/', getActivitiesByDiscordId)
 router.get('/recent', getRecentActivitiesByDiscordId)
-
+router.get('/timeframe', getActivitiesByTimeframe)
+const date = new Date()
+date.getUTCDate()
 module.exports = router
